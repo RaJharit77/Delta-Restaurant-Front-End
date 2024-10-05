2cc0047b27666f841f8eb7340f755f5a3580d555
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-05 14:32:58 +0300

    feat: rename an import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import AboutPage from './pages/AboutPage';
 import Accueil from './pages/AccueilPage';
+import CommandePage from './pages/CommandePage';
 import ContactPage from './pages/ContactPage';
 import Footer from './pages/Footer';
 import MenuPage from './pages/MenuPage';
 import NavigationBar from './pages/NavBar';
 import ReservationPage from './pages/ReservationPage';
-import CommandePage from './pages/commandePage';
 
 function App() {
   return (
