7efddd33c79f96632180761fde2847b34a79ec62
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-21 21:35:21 +0300

    feat: updating commandes page

--- src/components/Commandes.jsx
+++ src/components/Commandes.jsx
@@ -4,6 +4,7 @@ import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 function Commande() {
     const [orderData, setOrderData] = useState({
         mealName: '',
+        softDrink: '',
         quantity: '',
         tableNumber: ''
     });
@@ -66,51 +67,53 @@ function Commande() {
                         <span>{responseMessage}</span>
                     </div>
                 )}
-                <form  className="w-full" onSubmit={handleSubmit}>
+                <form className="w-full" onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <input
                             type="text"
-                            id="mealName"
                             name="mealName"
-                            placeholder="Nom du repas et/ou boisson"
-                            className="w-full px-4 py-2 border border-creme rounded-md bg-transparent text-white"
                             value={orderData.mealName}
                             onChange={handleChange}
+                            placeholder="Nom du plat"
                             required
+                            className="w-full p-2 rounded"
                         />
                     </div>
-
+                    <div className="mb-4">
+                        <input
+                            type="text"
+                            name="softDrink"
+                            value={orderData.softDrink}
+                            onChange={handleChange}
+                            placeholder="Boisson"
+                            required
+                            className="w-full p-2 rounded"
+                        />
+                    </div>
                     <div className="mb-4">
                         <input
                             type="number"
-                            id="quantity"
                             name="quantity"
-                            placeholder="Quantité"
-                            className="w-full px-4 py-2 border border-creme rounded-md bg-transparent text-white"
                             value={orderData.quantity}
                             onChange={handleChange}
+                            placeholder="Quantité"
                             required
+                            className="w-full p-2 rounded"
                         />
                     </div>
-
                     <div className="mb-4">
                         <input
-                            type="text"
-                            id="tableNumber"
+                            type="number"
                             name="tableNumber"
-                            placeholder="Numéro de table"
-                            className="w-full px-4 py-2 border border-creme rounded-md bg-transparent text-white"
                             value={orderData.tableNumber}
                             onChange={handleChange}
+                            placeholder="Numéro de Table"
                             required
+                            className="w-full p-2 rounded"
                         />
                     </div>
-
-                    <button
-                        type="submit"
-                        className="w-full bg-creme text-black py-2 px-4 rounded hover:bg-or transition"
-                    >
-                        Passer la commande
+                    <button type="submit" className="w-full p-2 bg-creme text-black font-bold rounded hover:bg-white transition">
+                        Commander
                     </button>
                 </form>
             </div>
