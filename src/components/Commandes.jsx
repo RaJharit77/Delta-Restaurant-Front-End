e48d3e2dfa9873b9f497c9ebb2f3407426872cff
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-22 21:46:05 +0300

    feat: updating commandes pages

--- src/components/Commandes.jsx
+++ src/components/Commandes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 function Commande() {
