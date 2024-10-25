72e93ad0d49d01cfb98a0da8216530197c89486d
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-25 21:10:13 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
 
-const Notification = () => {
+const Notifications = () => { 
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
@@ -14,28 +15,26 @@ const Notification = () => {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600">
+        <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
             <p className="text-creme font-semibold">Visitez notre hôtel !</p>
             <p className="text-gray-900 mt-2">
                 Profitez d'une expérience inoubliable dans notre hôtel de rêve.
             </p>
-            <a
-                href="https://delta-hotel-madagascar.vercel.app"
-                target="_blank"
-                rel="noopener noreferrer"
+            <button
+                onClick={() => window.open("https://delta-hotel-madagascar.vercel.app", "_blank")}
                 className="inline-block mt-4 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
             >
                 En savoir plus
-            </a>
+            </button>
             <button
                 onClick={() => setIsVisible(false)}
                 className="absolute top-2 right-2 text-or hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
                 aria-label="Fermer la notification"
             >
-                &times;
+                <FaTimes />
             </button>
         </div>
     );
 };
 
-export default Notification;
+export default Notifications;
