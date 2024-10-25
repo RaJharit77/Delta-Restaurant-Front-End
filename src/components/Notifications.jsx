df622fa64d8952c9a61bd1aec553987c5f8ec649
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-25 19:09:38 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,3 +1,4 @@
+// src/components/Notification.jsx
 import React, { useEffect, useState } from 'react';
 
 const Notification = () => {
@@ -14,8 +15,8 @@ const Notification = () => {
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600">
-            <p className="text-creme font-semibold">Visitez notre hôtel !</p>
+        <div className="fixed bottom-5 right-5 bg-white shadow-lg rounded-lg p-4 w-80 border border-gray-600">
+            <p className="text-yellow-100 font-semibold">Visitez notre hôtel !</p>
             <p className="text-gray-900 mt-2">
                 Profitez d'une expérience inoubliable dans notre hôtel de rêve.
             </p>
@@ -23,13 +24,13 @@ const Notification = () => {
                 href="https://delta-hotel-madagascar.vercel.app"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block mt-4 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
+                className="inline-block mt-4 bg-yellow-100 text-black py-2 px-4 rounded-md hover:bg-yellow-300 transition duration-300 cursor-pointer"
             >
                 En savoir plus
             </a>
             <button
                 onClick={() => setIsVisible(false)}
-                className="absolute top-2 right-2 text-or hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
+                className="absolute top-2 right-2 text-yellow-300 hover:text-yellow-400 cursor-pointer bg-emerald-600"
             >
                 &times;
             </button>
