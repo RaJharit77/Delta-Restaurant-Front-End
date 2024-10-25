b6b15bb95d8a17b48c6fd9ada1bbf16a88d88745
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-26 00:02:00 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -17,12 +17,12 @@ const Notifications = () => {
     return (
         <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
             <p className="text-creme font-semibold text-lg text-left">Visitez notre hôtel !</p>
-            <p className="text-gray-900 mt-2 text-left">
+            <p className="text-gray-900 mt-3 text-left">
                 Profitez d'une expérience inoubliable dans notre hôtel de rêve.
             </p>
             <button
                 onClick={() => window.open("https://delta-hotel-madagascar.vercel.app", "_blank")}
-                className="inline-block mt-4 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
+                className="inline-block mt-5 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
             >
                 En savoir plus
             </button>
