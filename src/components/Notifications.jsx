22b8b59e0cf5013662dce3cce262e98ec1098bea
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-26 00:44:52 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -18,17 +18,17 @@ const Notifications = () => {
         <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600 z-50">
             <p className="text-creme font-semibold text-lg text-left">Visitez notre hôtel !</p>
             <p className="text-gray-900 mt-2 text-left">
-                Profitez d'une expérience inoubliable dans notre hôtel de rêve.
+                Vivez une expérience luxueuse et inoubliable dans notre hôtel, conçu pour votre confort et bien-être.
             </p>
             <button
                 onClick={() => window.open("https://delta-hotel-madagascar.vercel.app", "_blank")}
-                className="inline-block mt-4 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
+                className="inline-block mt-3 bg-creme text-black py-2 px-4 rounded-md hover:bg-or transition duration-300 cursor-pointer"
             >
                 En savoir plus
             </button>
             <button
                 onClick={() => setIsVisible(false)}
-                className="absolute top-2 right-2 text-or hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
+                className="absolute top-2 right-2 text-creme hover:text-or bg-emerald-700 bg-opacity-50 cursor-pointer"
                 aria-label="Fermer la notification"
             >
                 <FaTimes />
