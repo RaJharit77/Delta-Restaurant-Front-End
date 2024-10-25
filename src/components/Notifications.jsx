aa295f18d493a9d8628802d3613ad4112383478d
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-25 17:41:32 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -16,7 +16,7 @@ const Notification = () => {
     return (
         <div className="fixed bottom-5 right-5 bg-white bg-opacity-50 shadow-lg rounded-lg p-4 w-80 border border-gray-600">
             <p className="text-creme font-semibold">Visitez notre hôtel !</p>
-            <p className="text-gray-300 mt-2">
+            <p className="text-gray-800 mt-2">
                 Profitez d'une expérience inoubliable dans notre hôtel de rêve.
             </p>
             <a
