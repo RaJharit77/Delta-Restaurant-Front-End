89c929a3a359930e78f8e23a70acc1d6d458455e
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-25 17:06:08 +0300

    feat: adding new a feature

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,38 +1,35 @@
-// src/components/Notification.jsx
 import React, { useEffect, useState } from 'react';
 
 const Notification = () => {
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
-        // Affiche la notification après 2 secondes
         const timer = setTimeout(() => {
             setIsVisible(true);
-        }, 2000);
+        }, 3000);
 
-        // Nettoyage au démontage du composant
         return () => clearTimeout(timer);
     }, []);
 
     if (!isVisible) return null;
 
     return (
-        <div className="fixed bottom-5 right-5 bg-white shadow-lg rounded-lg p-4 w-80 border border-gray-200">
-            <p className="text-gray-800 font-semibold">Visitez notre hôtel !</p>
-            <p className="text-gray-600 mt-2">
+        <div className="fixed bottom-5 right-5 bg-gray-300 shadow-lg rounded-lg p-4 w-80 border border-gray-200">
+            <p className="text-creme font-semibold">Visitez notre hôtel !</p>
+            <p className="text-emerald mt-2">
                 Profitez d'une expérience inoubliable dans notre hôtel de rêve.
             </p>
             <a
-                href="https://votre-hotel.com"
+                href="https://delta-hotel-madagascar.vercel.app"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block mt-4 bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300"
+                className="inline-block mt-4 bg-creme text-white py-2 px-4 rounded-md hover:bg-or transition duration-300"
             >
                 En savoir plus
             </a>
             <button
                 onClick={() => setIsVisible(false)}
-                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                className="absolute top-2 right-2 text-emerald-600 hover:text-emerald-700"
             >
                 &times;
             </button>
